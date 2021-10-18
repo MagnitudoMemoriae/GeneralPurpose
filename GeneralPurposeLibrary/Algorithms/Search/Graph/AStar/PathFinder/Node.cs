82c0071077c828d7d54151b185c74f7c84bf5de6
WildBoar <ugo.marchesini@libero.it>
2021-10-18 17:01:52 +0200

    Add AStar Search Algorithm

---
+++ GeneralPurposeLibrary/Algorithms/Search/Graph/AStar/PathFinder/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace GeneralPurposeLibrary.Algorithms.Search.Graph.AStar.PathFinder
+{
+
+
+
+    public abstract class Node
+    {
+        public readonly Point Position;
+    }
+
+
+
+
+    public abstract class Cell
+    {
+            
+    }
+
+    public class CellSimple
+    {
+
+    }
+
+    public class CellDirection
+    {
+
+    }
+}
